5cca9662082d6696a666b82cf6345b21ea8ccc6b
othmane bakkass <othmanebakkass@gmail.com>
2024-08-26 14:59:33 +0100

    feat: setup env variables for youtube api key and setup validation for envs vars

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,11 @@
+import createJiti from "jiti";
+import { fileURLToPath } from "node:url";
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env files so they can be validated during build
+jiti("./env");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
